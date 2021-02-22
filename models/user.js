5b2f3b8d36e3f008e449308e99f2c12d7c938ac3
TAT3MILLER <47550949+TAT3MILLER@users.noreply.github.com>
2021-02-22 00:30:22 -0500

    login fixed
    login fixed

--- models/user.js
+++ models/user.js
@@ -22,5 +22,8 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+
+
+
 const User = mongoose.model('User', userSchema)
 module.exports = User
--- passportconfig.js
+++ passportconfig.js
@@ -1,38 +1,39 @@
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
-const User = require("./models/user")
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require("./models/user");
 
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      //match the user
+      User.findOne({ email: email })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, { message: 'Email not registered' });
+          }
 
-function initialize(passport, getUserByEmail, getUserById) {
-    const authenticateUser = async (email, password, done) => {
-        const user = getUserByEmail(email)
-        if (user == null) {
-            return done(null, false, {message: 'No user with that email'})
-        }
+          //match the password
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
 
-        try {
-            if (await bcrypt.compare(password, user.password)){
-                return done(null, user)
-
-            }else{
-                return done(null, false, { message: 'Password incorrect' })
-
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'Incorrect Password' });
             }
-        } catch (e) {
-            return done(e)
-
-        }
-
-    }
-    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-
-    passport.serializeUser((user, done) => {
-        return done(null, user.id)
+          });
+        })
+        .catch((err) => { console.log(err) });
     })
-    passport.deserializeUser((id, done) => {
-        return done(null, getUserById(id))
-    })
+  );
 
-}
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
 
-module.exports = initialize
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+};
