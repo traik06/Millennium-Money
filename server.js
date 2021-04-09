90cf33923fd89d866cbd8194028a273f6d86b645
TAT3MILLER <47550949+TAT3MILLER@users.noreply.github.com>
2021-04-08 21:39:30 -0400

    Updated server.js
    server.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
 
 const express = require('express')
 const app = express()
@@ -53,11 +50,19 @@ app.use('/', require('./routes/feedback'));
 app.use('/', require('./routes/prevExpense'));
 
 // connect mongoose to db
-mongoose.connect(process.env.DATABASE_URL, { userNewURLParser: true })
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.error('Connected to Mongoose'))
+var mongoPassword = 'hello123';
+			
+var http = require('http');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
+  var config = JSON.parse(process.env.APP_CONFIG);
+  var MongoClient = require('mongodb').MongoClient;
 
-// app port
-app.listen(process.env.PORT || 3000)
+  MongoClient.connect(
+    "mongodb://" + config.mongo.user + ":" + encodeURIComponent(mongoPassword) + "@" + 
+    config.mongo.hostString, 
+    
+  );
+});
+server.listen(process.env.PORT);
