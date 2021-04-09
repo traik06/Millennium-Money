8880c802187e65076460210f995e74bec1a44c08
TAT3MILLER <47550949+TAT3MILLER@users.noreply.github.com>
2021-04-08 23:15:36 -0400

    stuff bein weird
    weird

--- server.js
+++ server.js
@@ -51,18 +51,27 @@ app.use('/', require('./routes/prevExpense'));
 
 // connect mongoose to db
 var mongoPassword = 'hello123';
-			
+
+
 var http = require('http');
 var server = http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  var config = JSON.parse(process.env.APP_CONFIG);
-  var MongoClient = require('mongodb').MongoClient;
 
-  MongoClient.connect(
+  var config = JSON.parse(process.env.APP_CONFIG);
+  
+
+  mongoose.connect(
     "mongodb://" + config.mongo.user + ":" + encodeURIComponent(mongoPassword) + "@" + 
-    config.mongo.hostString, 
+    config.mongo.hostString, { userNewURLParser: true }
     
   );
+  const db = mongoose.connection
+  const connection = mongoose.connection;
+  connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 });
-server.listen(process.env.PORT);
+
+
+app.listen(process.env.PORT);
